859a74d4a10abe6dba4dbcc4304f1320675593fa
Austin Amani <amaniaustin123@gmail.com>
2024-04-19 11:48:21 +0300

    Completed the React Starter For Now

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App(){
+
+    return(
+        <div>
+            <p className="text-blue-300">Testing Text</p>
+        </div>
+    )
+}
+
+export default App
